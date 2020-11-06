b3af796fb83f7f58f2dc2c0dfdba9d4ba5c16702
Prabhakar Kumar Panday <prabhakarpanday4@gmail.com>
2020-11-06 08:08:41 +0100

    Add files via upload
    Updating the comments

--- ImageReader.h
+++ ImageReader.h
@@ -1,18 +1,18 @@
-#pragma once
+#pragma once   //current source file to be included only once in a single compilation
 
-#include "ImageFile.h"
-#include <string>
+#include "ImageFile.h"  //include the ImageFile.h File
+#include <string>        //Basic I/O includes
 #include <fstream>
 #include <iostream>
 
-class Reader {
-public:
-	void readImageFileInfo(std::string);
-	void readImageFile();
-	Reader();
-	ImageFile getImageFile();
-private:
-	ImageFile img;
-	std::ifstream inStream;
-	std::string fileName;
+class Reader {           //class to read the image
+public:                  //All the Declarations Public
+	void readImageFileInfo(std::string);    //Reads the Image file Header Values
+	void readImageFile();                   //Reads the image Pixel Values
+	Reader();								//Instance of the Reader used in imagereader.cpp
+	ImageFile getImageFile();				//To get the image File
+private:				//All the Declarations Private
+	ImageFile img;		//Instance of the imageFile img, used in imagereader.cpp
+	std::ifstream inStream;		//for Reading a file
+	std::string fileName;		//Name of the file to be read, these 2 are passed from the calling function
 };
